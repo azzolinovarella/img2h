987cbf91a9ee763b2b69e38d1ca4463add4a16a7
Felipe Azzolino Varella <felipe_azzolinovarella@hotmail.com>
2022-07-26 00:40:24 -0300

    novo deploy para heroku

--- README.md
+++ README.md
@@ -10,8 +10,9 @@ https://img2h.herokuapp.com/
 - [X] CANCELADO - Centralizar ou aumentar botão de baixar imagem em app.py
 - [X] Colocar bottão de submmit no sprite (sem ser automático após passar parâmetro)
 - [X] Renomear projeto para Img2H
-- [ ] Subir no heroku
+- [X] Subir no heroku
 - [X] Colocar set_page_config no app (com favicon e nome de app)
 - [X] Criar logo app
-- [ ] Add licença e link pro projeto em app
+- [X] Add licença e link pro projeto em app
+- [ ] Melhorar texto e deixar mais claro
 -->
--- app/run.sh
+++ app/run.sh
@@ -3,7 +3,4 @@
 conda init > /dev/null 2> /dev/null && source ~/.bashrc
 conda activate ${APP_ENV}
 
-### Heroku
-# streamlit run --browser.serverAddress ${APP_URL} --browser.serverPort ${PORT} --server.port ${PORT} --theme.base dark app.py  # Heroku 
-### Local
-streamlit run --browser.serverAddress ${APP_URL} --browser.serverPort ${APP_PORT} --server.port ${APP_PORT} --theme.base dark app.py  # Local 
+streamlit run --browser.serverAddress ${APP_URL} --browser.serverPort ${PORT} --server.port ${PORT} --theme.base dark app.py  # Heroku 
