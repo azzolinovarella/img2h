131ecf09cde918008b7829131e94bcb647279046
Felipe Azzolino Varella <felipe_azzolinovarella@hotmail.com>
2022-07-26 01:58:19 -0300

    novo deploy para heroku

--- api/run.sh
+++ api/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-conda init > /dev/null 2> /dev/null && source ~/.bashrc
+# conda init > /dev/null 2> /dev/null && source ~/.bashrc
+conda init bash || source ~/.bashrc
 conda activate ${API_ENV}
 python api.py
--- app/run.sh
+++ app/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-conda init > /dev/null 2> /dev/null && source ~/.bashrc
+# conda init > /dev/null 2> /dev/null && source ~/.bashrc
+conda init bash || source ~/.bashrc
 conda activate ${APP_ENV}
 
 streamlit run --browser.serverAddress ${APP_URL} --browser.serverPort ${PORT} --server.port ${PORT} --theme.base dark app.py  # Heroku 
