736f8cd9568f1df01490c171d5090ad5efaf7613
Felipe Azzolino Varella <felipe_azzolinovarella@hotmail.com>
2022-07-26 12:14:39 -0300

    novo deploy para heroku

--- app/run.sh
+++ app/run.sh
@@ -2,5 +2,6 @@
 
 conda init > /dev/null 2> /dev/null && source ~/.bashrc
 conda activate app-env
-et -a && source .env && set +a  # Necessário para o heroku
-streamlit run --browser.serverAddress ${APP_URL} --browser.serverPort ${PORT} --server.port ${PORT} --theme.base dark app.py  # Heroku
+set -a && source .env && set +a  # Necessário para o heroku
+# streamlit run --browser.serverAddress ${APP_URL} --browser.serverPort ${PORT} --server.port ${PORT} --theme.base dark app.py  # Heroku
+streamlit run --server.enableCORS false --server.enableXsrfProtection false --server.port ${PORT} --theme.base dark app.py
