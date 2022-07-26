9d0a716ab1ca5fe97d26f460fa625dcc72edeeaf
Felipe Azzolino Varella <felipe_azzolinovarella@hotmail.com>
2022-07-26 13:00:36 -0300

    novo deploy para heroku

--- api/run.sh
+++ api/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 conda init > /dev/null 2> /dev/null && source ~/.bashrc
-# conda activate api-env
+conda activate api-env
 set -a && source .env && set +a  # Necessário para o heroku
 python api.py
