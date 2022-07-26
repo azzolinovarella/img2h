acf8ac38d4ff62758fc4cd3a8a5125419f429a1a
Felipe Azzolino Varella <felipe_azzolinovarella@hotmail.com>
2022-07-26 02:22:14 -0300

    novo deploy para heroku

--- api/run.sh
+++ api/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+conda activate ${API_ENV}
 python api.py
--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+conda activate ${APP_ENV}
 streamlit run --browser.serverAddress ${APP_URL} --browser.serverPort ${PORT} --server.port ${PORT} --theme.base dark app.py  # Heroku 
